51aaa68c46efaf85584b977617b781068d3ed2fb
torufuruya <t.furuya825@gmail.com>
2025-08-20 14:52:15 +0900

    Initial commit

---
+++ LinkStaticSampleTests/LinkStaticSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  LinkStaticSampleTests.swift
+//  LinkStaticSampleTests
+//
+//  Created by Toru Furuya on 2025/08/20.
+//
+
+import Testing
+@testable import LinkStaticSample
+
+struct LinkStaticSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
